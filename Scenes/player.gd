ebbce978a67134e43dd4620533d17f91f12f0dbb
DaviSN <111313463+DaviSN2006@users.noreply.github.com>
2025-10-18 19:41:35 -0400

    Script

---
+++ Scenes/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speed = 200
+
+func _physics_process(delta):
+	
+	velocity.x = 0
+	velocity.y = 0
+	
+	# Input básico
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += speed
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= speed
+	
+	move_and_collide(speed * delta)
